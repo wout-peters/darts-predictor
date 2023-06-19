0cd5bd92de052cc9002aa4ff2d1ae70e46183d24
woutp <wout.peters6@gmail.com>
2023-06-19 16:31:00 +0200

    first steps to web scraper finished

--- test.py
+++ test.py
@@ -5,12 +5,25 @@ url = 'https://clickondarts.com/Darts/Results/8371/M/PDC-World-Championship-2023
 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
-year_element = soup.find(id='mainContentPlaceHolder_cboYear')
-year_selected = year_element.select('[selected]')
-for year in year_selected:
-    print(year.text)
 
-#year_selected = year_element.select('option[selected]')
-#print(year_selected)
-#year = year_selected.get('value')
-#print(year)
+text = []
+data = soup.select('div[class*="row"]')
+for elements in data:
+    rowText = elements.text
+    if (len(rowText) > 0):
+        text.append(rowText)
+
+index = next((i for i, s in enumerate(text) if 'Finalist:' in s), None)
+text = text[index+3:]
+
+index = next((i for i, s in enumerate(text) if 'YearWinnerRunner-Up' in s), None)
+text = text[:index-1]
+
+print(text)
+#for elements in dates_elements:
+#    dates_selected = elements.select("span[id*=lblFixtureDate_]")
+#    for dates in dates_selected:
+#        #dateList.append(dates.text)
+#        dateMatches.append(dates['id'].split('_')[-1] + 1)
+
+    
