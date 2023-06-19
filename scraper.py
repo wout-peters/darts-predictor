c7a3651d976bb7ab5015356c2ac8d128ef08954f
woutp <wout.peters6@gmail.com>
2023-06-19 11:28:44 +0200

    first steps for webdriver

--- scraper.py
+++ scraper.py
@@ -1,1 +1,17 @@
 import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+#Initiate browser
+service = ChromeService(executable_path="C:\\Users\\Wout Peters\\webdriver\\chromedriver.exe")
+browser = webdriver.Chrome(service = service)
+#browser.get('https://www.instagram.com/')
+
+
+
+
+browser.quit()
+
+#URL = "https://clickondarts.com/DartsStats.aspx"
+#page = requests.get(URL)
+#print(page.text)
